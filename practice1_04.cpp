776add107c8b300212be3090a4123e24ee385278
Chuxia2 <87934625+Chuxia2@users.noreply.github.com>
2021-08-31 13:51:39 +0800

    Add files via upload

---
+++ practice1_04.cpp
@@ -0,0 +1,40 @@
+﻿// practice1_04.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+using namespace std;
+
+template <class T>
+T& inner_product(const T arr[], const T brr[], const int n, T& result);
+
+int main()
+{
+    int result = 0;
+    int arr[5] = { 1,2,3,4,5 };
+    int brr[5] = { 1,2,3,4,5 };
+    inner_product(arr, brr, 5, result);
+    cout << "result = " << result << endl;
+    return 0;
+}
+
+template <class T>
+T& inner_product(const T arr[], const T brr[], const int n, T& result)
+{
+    for (int i = 0; i < n; i++)
+    {
+        result += arr[i] * brr[i];
+    }
+    return result;
+}
+
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
---
+++ practice1_05.cpp
@@ -0,0 +1,41 @@
+﻿// practice1_05.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+using namespace std;
+
+template <class T>
+T iota(T arr[], const T value, const int n);
+
+int main()
+{
+    int arr[5] = { 0 };
+    iota(arr, 5, 5);
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << '\t';
+    }
+    cout << endl;
+    return 0;
+}
+
+template <class T>
+T iota(T arr[], const T value, const int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = value + i;
+    }
+    return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
